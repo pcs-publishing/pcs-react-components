ec11388c67cebd20f302f4c83c7abfa2679a1646
Christopher Train <ctrain@pcs-publishing.com>
2021-01-08 13:28:50 +0000

    Update react and react-dom

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,6 +2,5 @@ import { useContext } from 'react'
 import { ThemeContext } from '../theme-styled'
 
 export default function useTheme() {
-  const themeContext = useContext(ThemeContext)
-  return themeContext
+ return useContext(ThemeContext)
 }
