477a780d02fa6cab4e4263e5f71d160d7ebf8660
Dan Mills <danmills@Dans-MacBook-Pro.local>
2021-06-03 16:21:03 +0100

    Generate form in correct component

--- src/components/FileFormUpload/components/ImageHolder.tsx
+++ src/components/FileFormUpload/components/ImageHolder.tsx
@@ -1,12 +1,10 @@
 import React, { useCallback, useState } from 'react'
 import styled from '../../../theme-styled'
 import CenteredImage from '../../Image/CenteredImage'
-import { Form, Icon } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
 import Confirm from '../../Popups/Confirm'
 import { FileData, FileFormDefinition } from '../FileFormUpload'
-import Dropdown from '../../Dropdown'
-import Input from '../../Input'
-import TextArea from '../../TextArea'
+import FileForm from './FileForm'
 
 interface ImageContainerProps {
   fileData: FileData
@@ -75,7 +73,11 @@ const ImageHolder = ({
           maxWidth={maxImageWidth ?? 300}
         />
       </CenteredImageContainer>
-      {generateFileForm(data, fileFormDefinitions, onChangeForm)}
+      <FileForm
+        fileData={fileData}
+        fileFormDefinitions={fileFormDefinitions}
+        onChangeForm={onChangeForm}
+      />
       {onDelete ? (
         <Confirm
           close={() => setConfirm(false)}
@@ -88,60 +90,6 @@ const ImageHolder = ({
       ) : null}
     </ImageContainer>
   )
-}
-
-const generateFileForm = (
-  data: { [key: string]: any },
-  fileFormDefinitions?: FileFormDefinition[],
-  onChangeForm?: (key: string, value: any) => void
-) => {
-  if (!fileFormDefinitions || !onChangeForm) return null
-  return (
-    <Form>
-      {fileFormDefinitions.map((definition) =>
-        getFileFormDefinitionComponent(definition, onChangeForm, data)
-      )}
-    </Form>
-  )
-}
-
-const getFileFormDefinitionComponent = (
-  fileFormDefintion: FileFormDefinition,
-  onChangeForm: (key: string, value: any) => void,
-  data: { [key: string]: any }
-) => {
-  const onChange = (value: any) =>
-    onChangeForm(fileFormDefintion.dataName ?? fileFormDefintion.label, value)
-
-  const value = data[fileFormDefintion.dataName ?? fileFormDefintion.label]
-
-  switch (fileFormDefintion.type) {
-    case 'dropdown':
-      return (
-        <Dropdown
-          onChange={onChange}
-          value={value}
-          label={fileFormDefintion.label}
-          options={[{ key: 1, text: 'Option 1', value: 'option1' }]}
-        />
-      )
-    case 'input':
-      return (
-        <Input
-          onChange={onChange}
-          value={value}
-          label={fileFormDefintion.label}
-        />
-      )
-    case 'textarea':
-      return (
-        <TextArea
-          value={value}
-          label={fileFormDefintion.label}
-          onChange={onChange}
-        />
-      )
-  }
 }
 
 export default ImageHolder
